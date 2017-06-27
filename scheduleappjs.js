d53440cb1bbe5919b4c274c1f9bcdcc1f5fa419d
akul <akul.sharma007@gmail.com>
2017-06-27 11:50:13 +0530

    persisting data in json format through $http service

--- scheduleappjs.js
+++ scheduleappjs.js
@@ -1,25 +1,39 @@
 angular.module("scheduleapp",[])
 		.controller("func",func);
 
-function func(){
+function func($http){
 	this.addvar;
+	this.dbvar;
 	this.flag=false;
+	this.flagdb=false;
 	this.arr=[];
 	this.addfunc=function(){
 		this.arr.push(this.addvar);
+		this.flagdb=true;
 		this.addvar="";
 	}
 
 	this.removefunc=function(index){
 		this.arr.splice(index,1);
+		if(this.arr.length===0)
+			this.flagdb=false;
 	}
 
 	this.editfunc=function(){
 		this.flag=true;
 	}
 
-	this.savefunc=function(){
+	this.donefunc=function(){
 		this.flag=false;
 	}
 
+	this.dbsave=function(){
+
+		$http.post('http://localhost:3000/schedules',this.arr)
+		.then(function(response){
+			console.log("successfully saved");
+		});
+	}
 }
+
+
